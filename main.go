638b699efc110c8840abe6e93d1b5a0eeeead2b9
fercevik729 <ercevik.furkan@gmail.com>
2022-02-18 16:27:05 -0800

    GET request functionality done

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
-	"io"
 	"log"
+	"net/http"
 	"os"
 	"strings"
+	"sync"
+	"time"
 )
 
 func main() {
@@ -17,13 +19,13 @@ func main() {
 	reqs := flag.String("reqs", "./requests/http.txt", "Source for HTTP requests")
 	flag.Parse()
 
-	runLoad(*iters, *reqs)
+	runWave(*iters, *reqs)
 
 	fmt.Println("")
 }
 
-// runLoad runs all the specified http requests in the inFile
-func runLoad(iterations int, inFile string) []io.ReadCloser {
+// runWave runs all the specified http requests in the inFile
+func runWave(iterations int, inFile string) {
 
 	// Gather all the requests and request data
 	f, err := os.Open(inFile)
@@ -38,20 +40,54 @@ func runLoad(iterations int, inFile string) []io.ReadCloser {
 	for scanner.Scan() {
 		text := strings.Split(scanner.Text(), " ")
 
-		// TODO: read body data
+		// TODO: read body data for post and delete requests
 		requests = append(requests, request{
 			reqType:  text[0],
 			endpoint: text[1],
-			body:     text[2],
 		})
 
 	}
 
-	return nil
+	runRequests(iterations, requests)
+
+}
+
+func runRequests(its int, reqs []request) {
+
+	fmt.Printf("Running %d requests for %d sets:\n", len(reqs), its)
+	start := time.Now()
+	var wg sync.WaitGroup
+	for i := 0; i < its; i++ {
+
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for _, req := range reqs {
+
+				fmt.Printf("Running request: %s\n", req)
+				switch req.reqType {
+				case "GET":
+					resp, err := http.Get(req.endpoint)
+					if err != nil {
+						log.Printf("Request %s timed out\n", req)
+					} else {
+						log.Printf("Status code for %s: %d\n", req, resp.StatusCode)
+					}
+
+				default:
+					log.Printf("Invalid request: %s\n", req)
+				}
+			}
+		}()
+	}
+	wg.Wait()
+	log.Printf("Time to execute: %s\n", time.Since(start))
+}
+func (r request) String() string {
+	return fmt.Sprintf("Request type: %s, Endpoint: %s", r.reqType, r.endpoint)
 }
 
 type request struct {
 	reqType  string
 	endpoint string
-	body     string
 }
